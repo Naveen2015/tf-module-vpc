8b35a3142d13790c2875df3c76102b02bd000ffe
naveen <naveenkumar9707@gmail.com>
2024-08-10 15:25:59 -0500

    subnet creation

--- main.tf
+++ main.tf
@@ -3,3 +3,17 @@ resource "aws_vpc" "main" {
   tags = merge(var.tags, {Name="${var.env}-vpc"})
 
 }
+
+module "subnets"
+{
+  source="./subnets"
+
+  vpc_id = aws_vpc.main.id
+  for_each = var.subnets
+  cidr_block = each.value["cidr_block"]
+  azs = each.value["azs"]
+  tags=var.tags
+  env = var.env
+  name = each.value["name"]
+
+}
