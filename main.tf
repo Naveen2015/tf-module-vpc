25182033bf7663d5a0a2166513388d5f5b16f493
naveen <naveenkumar9707@gmail.com>
2024-08-10 15:45:23 -0500

    igw

--- main.tf
+++ main.tf
@@ -14,3 +14,9 @@ module "subnets" {
   env = var.env
   name = each.value["name"]
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, {Name="${var.env}-igw"})
+}
