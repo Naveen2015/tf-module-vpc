2995b7f690ec71208543ae15997b8fe9755c64ee
naveen <naveenkumar9707@gmail.com>
2024-08-11 13:48:43 -0500

    associate ngw to private subnets

--- main.tf
+++ main.tf
@@ -23,7 +23,6 @@ resource "aws_internet_gateway" "igw" {
 
 resource "aws_eip" "ngw" {
   count= length(var.subnets["public"].cidr_block)
-
   vpc   = true
   tags = merge(var.tags, {Name="${var.env}-ngw"})
 }
@@ -46,10 +45,14 @@ resource "aws_route" "igw" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route" "ngw" {
+/*resource "aws_route" "ngw" {
   count = length(local.all_private_subnet_ids)
   route_table_id = local.all_private_subnet_ids[count.index]
   nat_gateway_id = aws_nat_gateway.ngw.id
   destination_cidr_block = "0.0.0.0/0"
+}*/
+
+output "ngw" {
+  value = aws_nat_gateway.ngw
 }
 
